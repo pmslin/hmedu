6cbf88546fddadd8ef48c238f98f928794ca8622
pmslin <344804202@qq.com>
2018-01-19 09:54:36 +0800

    课程等页面pb

--- src/AppBundle/Extension/TaskToolbarExtension.php
+++ src/AppBundle/Extension/TaskToolbarExtension.php
@@ -13,18 +13,18 @@ class TaskToolbarExtension extends Extension
                 'icon' => 'es-icon-menu',
                 'action' => 'course_task_show_plugin_task_list',
             ),
-            array(
-                'code' => 'note',
-                'name' => '笔记',
-                'icon' => 'es-icon-edit',
-                'action' => 'course_task_plugin_note',
-            ),
-            array(
-                'code' => 'question',
-                'name' => '问答',
-                'icon' => 'es-icon-help',
-                'action' => 'course_task_plugin_threads',
-            ),
+//            array(
+//                'code' => 'note',
+//                'name' => '笔记',
+//                'icon' => 'es-icon-edit',
+//                'action' => 'course_task_plugin_note',
+//            ),
+//            array(
+//                'code' => 'question',
+//                'name' => '问答',
+//                'icon' => 'es-icon-help',
+//                'action' => 'course_task_plugin_threads',
+//            ),
         );
     }
 }
